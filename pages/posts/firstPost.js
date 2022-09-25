5ea84d5e8c71a6e2e4437eb555daa7a50e94a1ee
Artsy Biba <biba.artsiom@gmail.com>
2022-09-25 10:45:41 -0400

    add links

---
+++ pages/posts/firstPost.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function FirstPost() {
+    return (
+        <>
+            <Link href="/">Home</Link>
+            <h1>First Post</h1>
+        </>
+    );
+}
